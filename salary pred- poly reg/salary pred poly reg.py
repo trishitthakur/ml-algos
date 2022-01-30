dcc509d7666c6be5c03191eef639ca99400a5432
trishitthakur <trishit30@gmail.com>
2022-01-30 12:23:01 -0800

    ml

--- salary pred- poly reg/salary pred poly reg.py
+++ salary pred- poly reg/salary pred poly reg.py
@@ -1,42 +1,64 @@
+'''from the machine learning A-Z course
+   coded by trishit nath thakur '''
+
+#Importing libraries
+
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+
 # Importing dataset
+
 dataset = pd.read_csv('Position_Salaries.csv')
 X = dataset.iloc[:, 1:-1].values
 y = dataset.iloc[:, -1].values
 
+
 # Training the Linear Regression model on whole dataset
-from sklearn.linear_model import LinearRegression
-lin_reg = LinearRegression()
-lin_reg.fit(X, y)
+
+from sklearn.linear_model import LinearRegression 
+lin_reg = LinearRegression()   #regressor object created
+lin_reg.fit(X, y)    #object fitted on the dataset
+
 
 # Training the Polynomial Regression model on the whole dataset
+
 from sklearn.preprocessing import PolynomialFeatures
-poly_reg = PolynomialFeatures(degree = 4)
-X_poly = poly_reg.fit_transform(X)
-lin_reg_2 = LinearRegression()
-lin_reg_2.fit(X_poly, y)
+poly_reg = PolynomialFeatures(degree = 4)  
+
+    # y = bo + b1x1 + b2x2^2 +... bnxn^n, degree is the n in above line
+
+X_poly = poly_reg.fit_transform(X) # to change matrix of single feature to n features 
+lin_reg_2 = LinearRegression() #object created
+lin_reg_2.fit(X_poly, y) #object fitted on the transformed dataset
+
 
 # Visualising the Linear Regression results
-plt.scatter(X, y, color = 'red')
-plt.plot(X, lin_reg.predict(X), color = 'blue')
+
+plt.scatter(X, y, color = 'red')     #plot for the real values
+plt.plot(X, lin_reg.predict(X), color = 'blue')   #plot for the predicted values
 plt.title('Truth or Bluff (Linear Regression)')
 plt.xlabel('Position Level')
 plt.ylabel('Salary')
 plt.show()
 
+
 # Visualising Polynomial Regression results
-plt.scatter(X, y, color = 'red')
-plt.plot(X, lin_reg_2.predict(poly_reg.fit_transform(X)), color = 'blue')
+
+plt.scatter(X, y, color = 'red')       #plot for the real values
+plt.plot(X, lin_reg_2.predict(poly_reg.fit_transform(X)), color = 'blue')   #plot for the predicted poly reg model
 plt.title('Truth or Bluff (Polynomial Regression)')
 plt.xlabel('Position level')
 plt.ylabel('Salary')
 plt.show()
 
+
 # Predicting a new result with Linear Regression
+
 lin_reg.predict([[6.5]])
 
+
 # Predicting a new result with Polynomial Regression
+
 lin_reg_2.predict(poly_reg.fit_transform([[6.5]]))
--- startup pred- multiple linear reg/startup pred multiple linear reg.py
+++ startup pred- multiple linear reg/startup pred multiple linear reg.py
@@ -1,28 +1,50 @@
+'''from the machine learning A-Z course
+   coded by trishit nath thakur'''
+
+#Importing libraries
+
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+
 # Importing dataset
+
 dataset = pd.read_csv('50_Startups.csv')
-X = dataset.iloc[:, :-1].values
+X = dataset.iloc[:, :-1].values 
 y = dataset.iloc[:, -1].values
 
+
 # Encoding categorical data
+
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder
-ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [3])], remainder='passthrough')
+ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [3])], remainder='passthrough') 
+
+  '''[3] is the column in which we apply encoding while remainder means all columns not 
+     specified in the list of “transformers” will be passed through without transformation'''
+
 X = np.array(ct.fit_transform(X))
 
+
 # Splitting the dataset
+
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+ 
+ # when random_state set to an integer, train_test_split will return same results for each execution.
 
 # Training Multiple Linear Regression model on the Training set
+
 from sklearn.linear_model import LinearRegression
-regressor = LinearRegression()
-regressor.fit(X_train, y_train)
+regressor = LinearRegression()  #creating object
+regressor.fit(X_train, y_train)  #object fitted on the dataset
+
 
 # Predicting Test set results
+
 y_pred = regressor.predict(X_test)
-np.set_printoptions(precision=2)
+np.set_printoptions(precision=2)  #numerical values with 2 decimal after comma
 print(np.concatenate((y_pred.reshape(len(y_pred),1), y_test.reshape(len(y_test),1)),1))
+
+ #to print real values and predicted values side by side to compare
